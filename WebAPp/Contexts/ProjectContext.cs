cbfae0d773d8b3867ba8ddbb4eba177e4e6ba397
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-09-26 22:46:13 +0300

    db schema created.

---
+++ WebAPp/Contexts/ProjectContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPp.Contexts
+{
+    public class ProjectContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options)
+            : base(options)
+        {
+
+        }
+    }
+
+    public class AppUser : IdentityUser<Guid>
+    {
+
+    }
+
+    public class AppRole : IdentityRole<Guid>
+    {
+
+    }
+
+}
