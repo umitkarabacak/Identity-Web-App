bc27b0306ffc8c43a166e1cfbd982dc57cc01981
Ümit Karabacak <umit.karabacak@hotmail.com.tr>
2022-09-26 23:00:10 +0300

    - default connection context created.

--- WebAPp/Program.cs
+++ WebAPp/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebAPp.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ProjectContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
 
 var app = builder.Build();
 
